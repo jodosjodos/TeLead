c03a5430f5f5bddad020476207552e94c70b414f
jodosjodos <jeandedieu2030@gmail.com>
2024-01-22 05:47:06 +0200

    add course model

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { MyLoggerModule } from './my-logger/my-logger.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { EmailModule } from './email/email.module';
+import { CourseModule } from './course/course.module';
 @Module({
   imports: [
     UserModule,
@@ -11,6 +12,7 @@ import { EmailModule } from './email/email.module';
     DatabaseModule,
     ConfigModule.forRoot({ isGlobal: true }),
     EmailModule,
+    CourseModule,
   ],
 })
 export class AppModule {}
