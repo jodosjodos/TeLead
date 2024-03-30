c63ca049cdc6a5b2732ad0e908dff0bbb62ac4ab
jodosjodos <jeandedieu2030@gmail.com>
2024-03-30 14:11:26 +0200

    [update] update enroll with intial  progress tracking

--- src/enrollments/enrollment.service.ts
+++ src/enrollments/enrollment.service.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { User } from '@prisma/client';
 import { DatabaseService } from 'src/database/database.service';
 
@@ -6,28 +10,59 @@ import { DatabaseService } from 'src/database/database.service';
 export class EnrollmentService {
   constructor(private readonly prisma: DatabaseService) {}
   async createEnroll(user: User, courseId: string) {
-    const course = await this.prisma.course.findUnique({
-      where: {
-        id: courseId,
-      },
-    });
-    if (!course)
-      throw new BadRequestException(' course with that id not found');
-    const alreadyEnrolled = await this.prisma.enrollment.findFirst({
-      where: {
-        courseId: course.id,
-        studentId: user.id,
-      },
-    });
-    if (alreadyEnrolled)
-      throw new BadRequestException(' student already enrolled');
-    const unrolledStudent = await this.prisma.enrollment.create({
-      data: {
-        courseId: course.id,
-        studentId: user.id,
-      },
-    });
-    console.log(unrolledStudent);
-    return unrolledStudent;
+    try {
+      const course = await this.prisma.course.findUnique({
+        where: {
+          id: courseId,
+        },
+      });
+      if (!course)
+        throw new BadRequestException(' course with that id not found');
+      const alreadyEnrolled = await this.prisma.enrollment.findFirst({
+        where: {
+          courseId: course.id,
+          studentId: user.id,
+        },
+      });
+      if (alreadyEnrolled)
+        throw new BadRequestException(' student already enrolled');
+      const unrolledStudent = await this.prisma.enrollment.create({
+        data: {
+          courseId: course.id,
+          studentId: user.id,
+        },
+      });
+      console.log(unrolledStudent);
+      return this.initializeProgressTracker(courseId, user.id);
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
+  }
+  async initializeProgressTracker(courseId: string, userId: string) {
+    try {
+      const chapters = await this.prisma.chapter.findMany({
+        where: {
+          courseId,
+        },
+      });
+      if (chapters.length == 0)
+        throw new BadRequestException("this course haven't  any chapters yet");
+      const progressPromises = chapters.map((chapter) => {
+        return this.prisma.chapterProgress.create({
+          data: {
+            chapterId: chapter.id,
+            progress: 0,
+            userId,
+            completed: false,
+          },
+        });
+      });
+      await Promise.all(progressPromises);
+      return {
+        msg: ' user have  enrolled successfully ',
+      };
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
   }
 }
